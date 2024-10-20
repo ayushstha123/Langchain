2a8696400a02f883cf7575b76a8e7af297a5ac13
Ayush Shrestha <60820123+ayushstha123@users.noreply.github.com>
2024-10-20 17:58:56 +0545

    text-splitters

---
+++ lesson1/LLM_L4_text-split.js
@@ -0,0 +1,80 @@
+// Text splitter 
+// we use text splitter due to limitation placed by llm so that  only specific information can get to llm 
+
+const { ChatGoogleGenerativeAI } = require("@langchain/google-genai");
+const { ChatPromptTemplate } = require("@langchain/core/prompts");
+const { createStuffDocumentsChain } = require("langchain/chains/combine_documents");
+const { RecursiveCharacterTextSplitter}=require("langchain/text_splitter")
+const { Document } = require("langchain/document");
+const https = require('https');
+const cheerio = require('cheerio');
+const dotenv = require("dotenv");
+
+async function scrapeWebpage(url) {
+    return new Promise((resolve, reject) => {
+        https.get(url, (res) => {
+            let htmlData = '';
+
+            res.on('data', (chunk) => {
+                htmlData += chunk;
+            });
+
+            res.on('end', () => {
+                const $ = cheerio.load(htmlData);
+                const content = [];
+                $('p, h1, h2, h3, h4, h5, h6').each((index, element) => {
+                    content.push($(element).text());
+                });
+                resolve(content.join(' '));
+            });
+
+        }).on('error', (err) => {
+            reject('Error fetching the webpage: ' + err);
+        });
+    });
+}
+
+dotenv.config();
+const model = new ChatGoogleGenerativeAI({
+    apiKey: process.env.GOOGLE_API,
+    temperature: 0.5,
+    maxOutputTokens: 1200,
+});
+
+async function main() {
+    const prompt = ChatPromptTemplate.fromTemplate(`
+    Answer the user's question based on the following context:
+    Context: {context}
+    
+    Question: {input}
+    
+    Answer:
+    `);
+
+    const chain = await createStuffDocumentsChain({
+        llm: model,
+        prompt,
+    });
+
+    const scrapedContent = await scrapeWebpage("https://commons.wikimedia.org/wiki/Main_Page");
+    // Create a Document object
+    const doc = new Document({ pageContent: scrapedContent });
+
+    const splitter=new RecursiveCharacterTextSplitter({
+        chunkSize:200,
+        chunkOverlap:20,
+    });
+
+    const splitDocs=await splitter.splitDocuments([doc]);
+
+    console.log(splitDocs);
+
+    const response = await chain.invoke({
+        input: "Summarize the page",
+        context: [doc]  // Pass an array with the document
+    });
+    // console.log(doc.pageContent.length);
+    // console.log(response);
+}
+
+main().catch(console.error);
