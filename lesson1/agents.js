5f2fe5ea5704b671a6367d8d6b0b582703c549e3
Ayush Shrestha <60820123+ayushstha123@users.noreply.github.com>
2024-10-23 23:40:55 +0545

    tavily search web

--- lesson1/agents.js
+++ lesson1/agents.js
@@ -1,10 +1,11 @@
 const { ChatGoogleGenerativeAI } = require("@langchain/google-genai");
 const { ChatPromptTemplate, MessagesPlaceholder } = require("@langchain/core/prompts");
 const { AgentExecutor, createOpenAIFunctionsAgent  } = require("langchain/agents");
+const {TavilySearchResults}= require("@langchain/community/tools/tavily_search");
 const dotenv = require("dotenv");
 dotenv.config();
 
-// Instantiate our model
+// Instantiate our model 
 const model = new ChatGoogleGenerativeAI({
     apiKey: process.env.GOOGLE_API,
     temperature: 0.7,
@@ -17,13 +18,17 @@ async function main() {
         new MessagesPlaceholder("agent_scratchpad"),
     ]);
 
+    //instantiate tavily search tool
+    const searchTools=new TavilySearchResults(
+        {
+            apiKey: process.env.TAVILY_API,}
+    );
     // Create and assign tools
     const tools = [
-        // Add your tools here, for example:
-        // new Calculator(),
-        // new WebBrowser(),
+        searchTools
     ];
-
+    
+    
     // Create agent
     const agent = await createOpenAIFunctionsAgent ({
         llm: model,
@@ -39,7 +44,7 @@ async function main() {
 
     // Call agent 
     const response = await agentExecutor.invoke({
-        input: "Hello, can you introduce yourself?"   
+        input: "Hello, what is the price of NIMB in NEPSE in 2022/october?"   
     });
 
     console.log(response);
